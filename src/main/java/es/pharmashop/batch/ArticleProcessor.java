d133818799cc7b96fb54fbbda4aaa6f28b3309d9
Alejandro Such Berenguer <alejandro.such@gmail.com>
2018-06-19 08:31:40 +0200

    fix

--- src/main/java/es/pharmashop/batch/ArticleProcessor.java
+++ src/main/java/es/pharmashop/batch/ArticleProcessor.java
@@ -12,28 +12,29 @@ import org.springframework.batch.item.ItemProcessor;
 @Builder
 @AllArgsConstructor
 public class ArticleProcessor implements ItemProcessor<Article, Article> {
-    private Float factor;
-    private Float margin;
-    private ProductRepository productRepository;
+  private Float factor;
+  private Float margin;
+  private ProductRepository productRepository;
 
-    @Override
-    public Article process(Article article) throws Exception {
-        Product product = productRepository.findByCnAndEan(article.getId(), article.getEan());
-        if(product == null) {
-            product = productRepository.save(Product.builder()
-              .cn(article.getId())
-              .ean(article.getEan())
-              .name(article.getDescription())
-              .revision(false)
-              .uniqueCode(null)
-              .build());
-        }
-
-        article.fixStock();
-        article.applyFactorToStock(factor);
-        article.applyMarginToPrice(margin);
-        article.setUniqueCode(product.getUniqueCode());
-
-        return article;
+  @Override
+  public Article process(Article article) throws Exception {
+    Product product = productRepository.findByCnAndEan(article.getId(), article.getEan());
+        
+    if (product == null) {
+      product = productRepository.save(Product.builder()
+        .cn(article.getId())
+        .ean(article.getEan())
+        .name(article.getDescription())
+        .revision(false)
+        .uniqueCode(null)
+        .build());
     }
+
+    article.fixStock();
+    article.applyFactorToStock(factor);
+    article.applyMarginToPrice(margin);
+    article.setUniqueCode(product.getUniqueCode());
+
+    return article;
+  }
 }
